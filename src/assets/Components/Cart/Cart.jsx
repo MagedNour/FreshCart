c0098dd86fa7161d2cdc3d398991bd4ab1c2cd24
MagedNour <magednour1995@gmail.com>
2024-09-04 20:13:48 +0300

    added fix to checkout path

--- src/assets/Components/Cart/Cart.jsx
+++ src/assets/Components/Cart/Cart.jsx
@@ -88,7 +88,7 @@ export default function Cart() {
                                         <p className="text-sm text-gray-700">including VAT</p>
                                     </div>
                                 </div>
-                                <Link to={`/shippingAddress/${cart.data._id}`} className="block text-center mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+                                <Link to={`/FreshCart/shippingAddress/${cart.data._id}`} className="block text-center mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
                                     Check out
                                 </Link>
                             </div>
