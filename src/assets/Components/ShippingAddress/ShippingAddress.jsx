f812349a6537d2df83ebcc8cd2d803d9afe203a6
MagedNour <magednour1995@gmail.com>
2024-09-04 20:26:44 +0300

    another fix

--- src/assets/Components/ShippingAddress/ShippingAddress.jsx
+++ src/assets/Components/ShippingAddress/ShippingAddress.jsx
@@ -31,7 +31,7 @@ export default function ShippingAddress(props) {
                 token: localStorage.getItem("token"),
             },
             params: {
-                url: "https://magednour.github.io/FreshCart/"
+                url: "https://magednour.github.io/FreshCart"
             }
         }).then(({ data }) => {
             setIsLoading(false)
